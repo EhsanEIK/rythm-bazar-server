634e6d1b8ae1eb382a50b4e4838b82f877fb5e2c
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-24 11:17:49 +0600

    database connection has been setup

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+require('dotenv').config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req, res) => {
     res.send('Rythm bazar server is running');
