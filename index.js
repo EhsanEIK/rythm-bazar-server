2065486d155ae0d56bcbb4510aefb41c70cf854b
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-24 11:13:32 +0600

    added middleware

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Rythm bazar server is running');
